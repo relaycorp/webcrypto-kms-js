b182cb6b04b20bcedf1a721ea5746dcde64a920a
Gus Narea <gnarea@users.noreply.github.com>
2023-04-05 02:34:34 +0900

    test: Refactor functional tests (#32)

--- src/functional_tests/aws.test.ts
+++ src/functional_tests/aws.test.ts
@@ -1,28 +1,24 @@
-import { KMSClient } from '@aws-sdk/client-kms';
+import { initKmsProviderFromEnv, KmsRsaPssProvider } from '../index';
+import { RSA_PSS_CREATION_ALGORITHM, RSA_PSS_SIGN_ALGORITHM } from '../testUtils/webcrypto';
+import { PLAINTEXT, verifyAsymmetricSignature } from './utils';
 
-import { AwsKmsRsaPssProvider } from '../lib/aws/AwsKmsRsaPssProvider';
-import { PLAINTEXT } from '../testUtils/stubs';
-import {
-  NODEJS_CRYPTO,
-  RSA_PSS_CREATION_ALGORITHM,
-  RSA_PSS_SIGN_ALGORITHM,
-} from '../testUtils/webcrypto';
-
-const CLIENT = new KMSClient({
-  credentials: { accessKeyId: 'accessKeyId', secretAccessKey: 'secretAccessKey' },
-  endpoint: 'http://localhost:8080',
-  region: 'eu-west-2',
+let provider: KmsRsaPssProvider;
+let keyPair: CryptoKeyPair;
+beforeAll(async () => {
+  provider = await initKmsProviderFromEnv('AWS');
+  keyPair = await provider.generateKey(RSA_PSS_CREATION_ALGORITHM, true, ['sign', 'verify']);
+});
+afterAll(async () => {
+  if (keyPair) {
+    await provider?.destroyKey(keyPair.privateKey);
+  }
+  await provider?.close();
 });
 
 test('AWS KMS', async () => {
-  const provider = new AwsKmsRsaPssProvider(CLIENT);
-  const keyPair = (await provider.generateKey(RSA_PSS_CREATION_ALGORITHM, true, [
-    'sign',
-    'verify',
-  ])) as CryptoKeyPair;
-  const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, keyPair.privateKey, PLAINTEXT);
+  const { publicKey, privateKey } = keyPair;
 
-  await expect(
-    NODEJS_CRYPTO.subtle.verify(RSA_PSS_SIGN_ALGORITHM, keyPair.publicKey, signature, PLAINTEXT),
-  ).resolves.toBe(true);
+  const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
+
+  await expect(verifyAsymmetricSignature(publicKey, signature, PLAINTEXT)).resolves.toBe(true);
 });
