9dde444606cf087258fe222b5773228e2e5e9983
Gus Narea <gnarea@users.noreply.github.com>
2025-04-16 14:35:34 +0100

    add tests

--- src/functional_tests/aws.test.ts
+++ src/functional_tests/aws.test.ts
@@ -1,5 +1,15 @@
+import { constants, createVerify, webcrypto } from 'crypto';
+import { KMSClient, VerifyCommand, SigningAlgorithmSpec, MessageType } from '@aws-sdk/client-kms';
+
+import { derSerializePublicKey, NODEJS_CRYPTO } from '../testUtils/webcrypto';
+import { derPublicKeyToPem } from '../testUtils/asn1';
 import { initKmsProviderFromEnv, KmsRsaPssProvider } from '../index';
-import { RSA_PSS_CREATION_ALGORITHM, RSA_PSS_SIGN_ALGORITHM } from '../testUtils/webcrypto';
+import {
+  KEY_USAGES,
+  RSA_PSS_CREATION_ALGORITHM,
+  RSA_PSS_IMPORT_ALGORITHM,
+  RSA_PSS_SIGN_ALGORITHM,
+} from '../testUtils/webcrypto';
 import { PLAINTEXT, verifyAsymmetricSignature } from './utils';
 
 process.env.KMS_ADAPTER = 'AWS';
@@ -12,7 +22,7 @@ let provider: KmsRsaPssProvider;
 let keyPair: CryptoKeyPair;
 beforeAll(async () => {
   provider = await initKmsProviderFromEnv('AWS');
-  keyPair = await provider.generateKey(RSA_PSS_CREATION_ALGORITHM, true, ['sign', 'verify']);
+  keyPair = await provider.generateKey(RSA_PSS_CREATION_ALGORITHM, true, KEY_USAGES);
 });
 afterAll(async () => {
   if (keyPair) {
@@ -21,10 +31,75 @@ afterAll(async () => {
   await provider?.close();
 });
 
-test('AWS KMS', async () => {
-  const { publicKey, privateKey } = keyPair;
+describe('AWS KMS', () => {
+  test('Peculiar WebCrypto verification', async () => {
+    const { publicKey, privateKey } = keyPair;
 
-  const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
+    const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
 
-  await expect(verifyAsymmetricSignature(publicKey, signature, PLAINTEXT)).resolves.toBe(true);
+    await expect(verifyAsymmetricSignature(publicKey, signature, PLAINTEXT)).resolves.toBe(true);
+  });
+
+  test('Node.js WebCrypto verification', async () => {
+    const { publicKey, privateKey } = keyPair;
+
+    const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
+
+    const publicKeySpki = await NODEJS_CRYPTO.subtle.exportKey('spki', publicKey);
+    const nodePublicKey = await webcrypto.subtle.importKey(
+      'spki',
+      publicKeySpki,
+      RSA_PSS_IMPORT_ALGORITHM,
+      true, // extractable
+      ['verify'], // key usages for public key
+    );
+    await expect(
+      webcrypto.subtle.verify(
+        RSA_PSS_SIGN_ALGORITHM,
+        nodePublicKey, // Use the re-imported key
+        Buffer.from(signature),
+        PLAINTEXT,
+      ),
+    ).resolves.toBeTrue();
+  });
+
+  test('Node.js verification', async () => {
+    const { publicKey, privateKey } = keyPair;
+
+    const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
+
+    const verify = createVerify('sha256');
+    verify.update(PLAINTEXT);
+    verify.end();
+    const publicKeyDer = await derSerializePublicKey(publicKey);
+    const publicKeyPem = derPublicKeyToPem(publicKeyDer);
+    expect(
+      verify.verify(
+        { key: publicKeyPem, padding: constants.RSA_PKCS1_PSS_PADDING },
+        new Uint8Array(signature),
+      ),
+    ).toBeTrue();
+  });
+
+  test('AWS SDK verification', async () => {
+    const { privateKey } = keyPair;
+
+    const signature = await provider.sign(RSA_PSS_SIGN_ALGORITHM, privateKey, PLAINTEXT);
+
+    const arnBuffer = (await provider.exportKey('raw', privateKey)) as ArrayBuffer;
+    const keyArn = Buffer.from(arnBuffer).toString();
+    const verifyCommand = new VerifyCommand({
+      KeyId: keyArn,
+      Message: PLAINTEXT,
+      MessageType: MessageType.RAW,
+      Signature: new Uint8Array(signature),
+      SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PSS_SHA_256,
+    });
+    const client = new KMSClient({
+      endpoint: process.env.AWS_KMS_ENDPOINT,
+      region: process.env.AWS_KMS_REGION,
+    });
+    const response = await client.send(verifyCommand);
+    expect(response.SignatureValid).toBe(true);
+  });
 });
